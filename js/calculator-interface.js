7b92bda71591f111dc0a4951eefc2e168c6ee52e
LD MacKrell <ldmackrell@gmail.com>
2017-10-19 00:20:33 -0700

    Update app to display error message if age is over expectancy.

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -4,26 +4,42 @@ import { Planet } from './../js/planet.js';
 $(document).ready(function() {
   $('.dob').submit(function(event) {
     event.preventDefault();
+    //clear UI
+    $('#output').text("");
+    $('#mercuryAge').text("");
+    $('#venusAge').text("");
+    $('#marsAge').text("");
+    $('#jupiterAge').text("");
+
+    $('#earthLeft').text("");
+    $('#mercuryLeft').text("");
+    $('#venusLeft').text("");
+    $('#marsLeft').text("");
+    $('#jupiterLeft').text("");
+
+    $('#error').text("");
+
     const born = new Date($('#born').val());
     let yourAge = new Calculator();
     let currentAge = yourAge.ageCalculator(born);
     let modelPlanet = new Planet(currentAge);
     const finalAge = (parseInt($('#death').val())) * 31536000;
-    console.log(finalAge);
     let deathPlanet = new Planet(currentAge, finalAge);
     //output for current age on different planets
-    $('#output').append(`You are ${currentAge} seconds old!`);
-    $('#mercuryAge').append(`You are ${modelPlanet.convertMercury()} years old on Mercury!`);
-    console.log(modelPlanet.convertMercury());
-    $('#venusAge').append(`You are ${modelPlanet.convertVenus()} years old on Venus!`);
-    $('#marsAge').append(`You are ${modelPlanet.convertMars()} years old on Mars!`);
-    $('#jupiterAge').append(`You are ${modelPlanet.convertJupiter()} years old on Jupiter!`);
+    $('#output').text(`You are ${currentAge} seconds old!`);
+    $('#mercuryAge').text(`You are ${modelPlanet.convertMercury()} years old on Mercury!`);
+    $('#venusAge').text(`You are ${modelPlanet.convertVenus()} years old on Venus!`);
+    $('#marsAge').text(`You are ${modelPlanet.convertMars()} years old on Mars!`);
+    $('#jupiterAge').text(`You are ${modelPlanet.convertJupiter()} years old on Jupiter!`);
     //output on anticipated life expectancy
-    $('#earthLeft').append(`Hail, Earthling! You have ${deathPlanet.lifeOnEarth()} years left to live! Act fast!`);
-    $('#mercuryLeft').append(`Hello, Mercurian! You have ${deathPlanet.lifeOnMercury()} years left to live! Act fast!`);
-    $('#venusLeft').append(`Salud, Venusian! You have ${deathPlanet.lifeOnVenus()} years left to live! Act fast!`);
-    $('#marsLeft').append(`Howdy, Martian! You have ${deathPlanet.lifeOnMars()} years left to live! Act fast!`);
-    $('#jupiterLeft').append(`Hiyah Jovian! You have ${deathPlanet.lifeOnJupiter()} years left to live! Act fast!`);
+    if (finalAge < currentAge) {
+      $('#error').text(`Wowee! You are older than you expected! Keep drinking those health shakes!`);
+    } else {
+      $('#earthLeft').text(`Hail, Earthling! You have ${deathPlanet.lifeOnEarth()} years left to live! Act fast!`);
+      $('#mercuryLeft').text(`Hello, Mercurian! You have ${deathPlanet.lifeOnMercury()} years left to live! Act fast!`);
+      $('#venusLeft').text(`Salud, Venusian! You have ${deathPlanet.lifeOnVenus()} years left to live! Act fast!`);
+      $('#marsLeft').text(`Howdy, Martian! You have ${deathPlanet.lifeOnMars()} years left to live! Act fast!`);
+      $('#jupiterLeft').text(`Hiyah Jovian! You have ${deathPlanet.lifeOnJupiter()} years left to live! Act fast!`);
+    }
   });
-
 });
--- js/calculator.js
+++ js/calculator.js
@@ -4,22 +4,7 @@ export class Calculator {
 
   ageCalculator(born) {
     let currentDate = Date.now();
-    console.log(Date.now());
     let elapsed = Math.floor((currentDate - born.getTime()) / 1000);
     return elapsed;
   }
-
 }
-//   currentAge() {
-//     let calculator = new Calculator(this.birthDate);
-//     let currentTime = $('#time').text(moment());
-//     let output = parseInt(currentTime - calculator);
-//   }
-// }
-
-// function ageCalculator(dob) {
-//   var timeStamp = Math.floor(Date.now()/ 1000);//should provide timestamp in seconds
-//   var date = new Date(timestamp);
-//   var seconds = date.getTime() / 1000;
-//
-// }
